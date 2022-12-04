267cb97638f5fc764dd097c3d740dac37fa5262a
JDSeiler <jseiler@radford.edu>
2022-12-04 14:47:44 -0500

    Run rustfmt

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -16,8 +16,8 @@ pub fn part_one(input: &str) -> Option<u32> {
 
             for item in f.as_bytes().iter() {
                 if second.binary_search(item).is_ok() {
-                    return *item
-                } 
+                    return *item;
+                }
             }
             // We will always find a duplicate, if we omit this `unreachable`
             // then the compiler rightly asserts that this `map` will sometimes
@@ -26,7 +26,7 @@ pub fn part_one(input: &str) -> Option<u32> {
         })
         .map(|duplicate_item| {
             // a-z => should map to 1-26
-            // A-Z => should map to 27-52 
+            // A-Z => should map to 27-52
 
             // Because capital letters come before lowercase letters in ASCII
             // We need two separate branches / two separate offsets.
@@ -35,9 +35,10 @@ pub fn part_one(input: &str) -> Option<u32> {
                 97..=122 => duplicate_item as u32 - 96,
                 // A-Z
                 65..=90 => duplicate_item as u32 - 38,
-                _ => unreachable!()
+                _ => unreachable!(),
             }
-        }).sum();
+        })
+        .sum();
 
     Some(total_priority)
 }
@@ -46,43 +47,45 @@ pub fn part_two(input: &str) -> Option<u32> {
     // Removing this allocation could speed up the solution quite a bit.
     // But `chunks` is not stabilized on normal iterators yet, only on slices.
     let lines: Vec<&str> = input.lines().collect();
-    let total_priority = lines.chunks(3).map(|group| {
-        // Allocating and using all these HashSets could also be very slow.
-        // But I'm not sure if there's actually a faster way, considering
-        // that in part_one I replaced a HashSet with binary search and it wasn't
-        // any faster.
-        let first: HashSet<char> = HashSet::from_iter(group[0].chars());
-        let second: HashSet<char> = HashSet::from_iter(group[1].chars());
-        let third: HashSet<char> = HashSet::from_iter(group[2].chars());
+    let total_priority = lines
+        .chunks(3)
+        .map(|group| {
+            // Allocating and using all these HashSets could also be very slow.
+            // But I'm not sure if there's actually a faster way, considering
+            // that in part_one I replaced a HashSet with binary search and it wasn't
+            // any faster.
+            let first: HashSet<char> = HashSet::from_iter(group[0].chars());
+            let second: HashSet<char> = HashSet::from_iter(group[1].chars());
+            let third: HashSet<char> = HashSet::from_iter(group[2].chars());
 
-        // If anything it makes the solution very readable.
-        for candidate in first.intersection(&second) {
-            if third.contains(candidate) {
-                return *candidate;
+            // If anything it makes the solution very readable.
+            for candidate in first.intersection(&second) {
+                if third.contains(candidate) {
+                    return *candidate;
+                }
             }
-        }
-        // At least one candidate WILL occur in `third`
-        unreachable!();
-    })
-    .map(|duplicate_item| {
-        // A final optimization for both parts would be to ditch iterators
-        // completely so that I can compute the sum as I'm calculating the 
-        // priorities. I am assuming the final `sum` call at the end of this
-        // iterator chain is another pass over every element that could be
-        // avoided.
+            // At least one candidate WILL occur in `third`
+            unreachable!();
+        })
+        .map(|duplicate_item| {
+            // A final optimization for both parts would be to ditch iterators
+            // completely so that I can compute the sum as I'm calculating the
+            // priorities. I am assuming the final `sum` call at the end of this
+            // iterator chain is another pass over every element that could be
+            // avoided.
 
-        // a-z => should map to 1-26
-        // A-Z => should map to 27-52 
+            // a-z => should map to 1-26
+            // A-Z => should map to 27-52
 
-        // Because capital letters come before lowercase letters in ASCII
-        // We need two separate branches / two separate offsets.
-        match duplicate_item {
-            'a'..='z' => duplicate_item as u32 - 96,
-            'A'..='Z' => duplicate_item as u32 - 38,
-            _ => unreachable!()
-        }
-    })
-    .sum();
+            // Because capital letters come before lowercase letters in ASCII
+            // We need two separate branches / two separate offsets.
+            match duplicate_item {
+                'a'..='z' => duplicate_item as u32 - 96,
+                'A'..='Z' => duplicate_item as u32 - 38,
+                _ => unreachable!(),
+            }
+        })
+        .sum();
 
     Some(total_priority)
 }
